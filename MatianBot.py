8177a127f0f76b5bfb1564669236f7cf953dbb5d
Matian Kyllönen <118250414+MatianKyllonen@users.noreply.github.com>
2024-01-22 11:01:46 +0200

    Matian

---
+++ MatianBot.py
@@ -0,0 +1,52 @@
+## ----- Ohjeet ----- ##
+#Koodin nimi tulee olla oma nimesi.
+#Botin nimen saat keksiä itse, mutta sen tulee olla asiallinen.
+#Botilla on kolme pakollista funktiota:
+#1. GetBool() -> Oma funktiosi, joka palauttaa totuusarvon.
+#Saat itse keksiä valinta perjaatteen.
+#2. SetData(MyData,OpponentData) -> Saat dataa meneillään olevasta taistelusta.
+#3. Restart() -> Resetoi saadun datan.
+#Resetointi tapahtuu aina ennen kun saat uuden vastustajan.
+#Resetoinnin tulee resetoida tiedot omista vanhoista liikkeistä ja vastustajan vanhoista liikkeista.
+#Saat myös resetoida muita muuttujia, joita itse olet asettanut.
+
+name = "MatianBot"
+
+MyMoves = []
+OpponentMoves = []
+
+def GetBool():
+    global MyMoves, MyMoves
+    #Tämän funktion tulee palauttaa True tai False.
+    #Saat itse valita miten ohjelma sen päättää.
+    #Käytössäsi on molempien pelaajien data aiemmilta kierroksilta.
+    #SetData-funktio asettaa listat, joissa on molempien pelaajien aiemmat liikeet.
+
+    falseAmount = 0
+
+    if len(MyMoves) == 0:
+        return True
+    if len(MyMoves) > 175:
+        return False
+    
+    for i in range(len(MyMoves)):
+        if MyMoves[i] == False:
+            falseAmount += 1
+    if(len(MyMoves) - falseAmount < falseAmount):
+        return True
+    return False   
+
+def Restart(): #Resets all the values for a new fight.
+    global MyMoves, OpponentMoves
+    MyMoves = []
+    OpponentMoves = []
+    #Jos jotain omaa resetoitavaa, niin laita se tähän.
+
+
+## ----- ÄLÄ KOSKE ALUE ----- ##
+
+#Älä muokkaa tai kutus tätä funktiota.
+def SetData(MyData,OpponentData):
+    global MyMoves, OpponentMoves
+    MyMoves = MyData
+    OpponentMoves = OpponentData   
