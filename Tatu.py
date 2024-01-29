e212f26b929c25919560e3316ece10642932d796
tatsart23 <133203646+tatsart23@users.noreply.github.com>
2024-01-29 09:00:16 +0200

    Add files via upload

---
+++ Tatu.py
@@ -0,0 +1,39 @@
+## ----- Ohjeet ----- ##
+#Koodin nimi tulee olla oma nimesi.
+#Botin nimen saat keksiä itse, mutta sen tulee olla asiallinen.
+#Botilla on kolme pakollista funktiota:
+#1. GetBool() -> Oma funktiosi, joka palauttaa totuusarvon.
+#Saat itse keksiä valinta perjaatteen.
+#2. SetData(MyData,OpponentData) -> Saat dataa meneillään olevasta taistelusta.
+#3. Restart() -> Resetoi saadun datan.
+#Resetointi tapahtuu aina ennen kun saat uuden vastustajan.
+#Resetoinnin tulee resetoida tiedot omista vanhoista liikkeistä ja vastustajan vanhoista liikkeista.
+#Saat myös resetoida muita muuttujia, joita itse olet asettanut.
+
+name = "Perttu"
+
+MyMoves = []
+OpponentMoves = []
+
+def GetInt():
+    global MyMoves, OpponentMoves
+     # Esimerkki yksinkertaisesta logiikasta: vaihtaa aina liikkeen vuorotellen.
+    if len(MyMoves) % 2 == 0:
+        return 0  # Valitse kivi
+    else:
+        return 1  # Valitse paperi
+
+def Restart(): #Resets all the values for a new fight.
+    global MyMoves, OpponentMoves
+    MyMoves = []
+    OpponentMoves = []
+    #Jos jotain omaa resetoitavaa, niin laita se tähän.
+
+
+## ----- ÄLÄ KOSKE ALUE ----- ##
+
+#Älä muokkaa tai kutus tätä funktiota.
+def SetData(MyData,OpponentData):
+    MyMoves = MyData
+    OpponentMoves = OpponentData    
+
