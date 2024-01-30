ab1de16f9e58536a61be4f83e4f394e1b69a75d8
KaikuGz <141708787+KaikuGz@users.noreply.github.com>
2024-01-30 10:57:42 +0200

    Add files via upload

---
+++ kaiku_kps.py
@@ -0,0 +1,45 @@
+## ----- Ohjeet ----- ##
+#Koodin nimi tulee olla oma nimesi.
+#Botin nimen saat keksiä itse, mutta sen tulee olla asiallinen.
+#Botilla on kolme pakollista funktiota:
+#1. GetBool() -> Oma funktiosi, joka palauttaa totuusarvon.
+#Saat itse keksiä valinta perjaatteen.
+#2. SetData(MyData,OpponentData) -> Saat dataa meneillään olevasta taistelusta.
+#3. Restart() -> Resetoi saadun datan.
+#Resetointi tapahtuu aina ennen kun saat uuden vastustajan.
+#Resetoinnin tulee resetoida tiedot omista vanhoista liikkeistä ja vastustajan vanhoista liikkeista.
+#Saat myös resetoida muita muuttujia, joita itse olet asettanut.
+
+name = "k4dd1m4771"
+
+MyMoves = []
+OpponentMoves = []
+
+def GetInt():
+    global MyMoves, OpponentMoves
+    #Tämän funktion tulee palutaa 0, 1 tai 2.
+    #Saat itse valita miten ohjelma sen päättää.
+    #Käytössäsi on molempien pelaajien data aiemmilta kierroksilta.
+    #SetData-funktio asettaa listat, joissa on molempien pelaajien aiemmat liikeet.
+    if(len(OpponentMoves) == 0):
+        return 0
+    if(OpponentMoves[len(OpponentMoves) == 0]):
+        return 0
+    if(OpponentMoves[len(OpponentMoves) == 1]):
+        return 1
+    if(OpponentMoves[len(OpponentMoves) == 2]):
+        return 2
+
+def Restart(): #Resets all the values for a new fight.
+    global MyMoves, OpponentMoves
+    MyMoves = []
+    OpponentMoves = []
+    #Jos jotain omaa resetoitavaa, niin laita se tähän.
+
+
+## ----- ÄLÄ KOSKE ALUE ----- ##
+
+#Älä muokkaa tai kutus tätä funktiota.
+def SetData(MyData,OpponentData):
+    MyMoves = MyData
+    OpponentMoves = OpponentData    
